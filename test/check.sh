9ef9aeef598b8ee7ca890f57d3257e272fb83c9f
Frank Heikens <fheikens@users.noreply.github.com>
2026-03-31 18:36:56 -0700

    [#44] Fix license header in test script
    The header in test/check.sh references pgmoneta and GPL-3.0 text.
    pgopr uses EPL-2.0. Replace the copied header with the correct
    EPL-2.0 notice matching the rest of the repository.

--- test/check.sh
+++ test/check.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
-## Copyright (C) 2026 The pgmoneta community
+## Eclipse Public License - v 2.0
 ##
-## This program is free software: you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation, either version 3 of the License, or
-## (at your option) any later version.
-##
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-## GNU General Public License for more details.
-##
-## You should have received a copy of the GNU General Public License
-## along with this program. If not, see <https://www.gnu.org/licenses/>.
+##   THE ACCOMPANYING PROGRAM IS PROVIDED UNDER THE TERMS OF THIS ECLIPSE
+##   PUBLIC LICENSE ("AGREEMENT"). ANY USE, REPRODUCTION OR DISTRIBUTION
+##   OF THE PROGRAM CONSTITUTES RECIPIENT'S ACCEPTANCE OF THIS AGREEMENT.
 set -euo pipefail
 
 readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
